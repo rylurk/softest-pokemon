ac66aef7b29775fcda55782ec789415ca3a3f3ab
ryan <ryanslh5@gmail.com>
2022-08-04 15:33:39 -0400

    Fixing incorrect browser assertion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 function getBaseUrl() {
-  if (typeof window === 'undefined') return ''; // Browser should use current path
+  if (typeof window !== 'undefined') return ''; // Browser should use current path
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
