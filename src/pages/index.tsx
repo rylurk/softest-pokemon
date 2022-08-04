d419d32908559670fc10ebac26f42019f104c096
ryan <ryanslh5@gmail.com>
2022-08-04 12:51:22 -0400

    Add trpc boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
-import type { NextPage } from 'next';
+import { trpc } from '../utils/trpc';
 
-const Home: NextPage = () => {
-  return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center">Which Pokémon is softer?</div>
-      <div className="p-2" />
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200" />
-        <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200" />
-      </div>
-    </div>
-  );
-};
+export default function Home() {
+  const { data, isLoading } = trpc.useQuery(['hello', { text: 'Ryan' }]);
 
-export default Home;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (data) {
+    return <div>{data.greeting}</div>;
+  }
+
+  // return (
+  //   <div className="h-screen w-screen flex flex-col justify-center items-center">
+  //     <div className="text-2xl text-center">Which Pokémon is softer?</div>
+  //     <div className="p-2" />
+  //     <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+  //       <div className="w-16 h-16 bg-red-200" />
+  //       <div className="p-8">Vs</div>
+  //       <div className="w-16 h-16 bg-red-200" />
+  //     </div>
+  //   </div>
+  // );
+}
