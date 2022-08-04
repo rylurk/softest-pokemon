82a882ba062d41696a5a98151a394a4bcfc3e742
ryan <ryanslh5@gmail.com>
2022-08-04 13:48:55 -0400

    Add random Pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,23 @@
-import { trpc } from '../utils/trpc';
+import { useEffect, useState } from 'react';
+import { getOptionsForVote } from '../utils/getRandomPokemon';
 
 export default function Home() {
-  const { data, isLoading } = trpc.useQuery(['hello', { text: 'Ryan' }]);
+  const [ids, setIds] = useState([0, 0]);
+  const [first, second] = ids;
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  useEffect(() => {
+    setIds(getOptionsForVote());
+  }, []);
 
-  if (data) {
-    return <div>{data.greeting}</div>;
-  }
-
-  // return (
-  //   <div className="h-screen w-screen flex flex-col justify-center items-center">
-  //     <div className="text-2xl text-center">Which Pokémon is softer?</div>
-  //     <div className="p-2" />
-  //     <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-  //       <div className="w-16 h-16 bg-red-200" />
-  //       <div className="p-8">Vs</div>
-  //       <div className="w-16 h-16 bg-red-200" />
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokémon is softer?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-800">{first}</div>
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
+      </div>
+    </div>
+  );
 }
