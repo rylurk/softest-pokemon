36847ab123b9acaa665aa0837f61699e05302b96
ryan <ryanslh5@gmail.com>
2022-08-04 18:13:42 -0400

    Styling and conditional pokemon rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getOptionsForVote } from '../utils/getRandomPokemon';
 import { trpc } from '../utils/trpc';
+import { inferQueryResponse } from './api/trpc/[trpc]';
 
 const btn =
   'inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500';
@@ -28,25 +28,29 @@ export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokémon is softer?</div>
-      <div className="p-2" />
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-64 h-64 flex flex-col items-center">
-          <img src={firstPokemon.data?.sprites.front_default!} className="w-full" />
-          <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
-          <button className={btn} onClick={() => voteForRoundest(first)}>
-            Softer
-          </button>
-        </div>
+      <div className="mt-2 p-8 flex justify-between max-w-2xl items-center">
+        {!firstPokemon.isLoading && firstPokemon.data && !secondPokemon.isLoading && secondPokemon.data && (
+          <PokemonListing pokemon={firstPokemon.data} vote={() => voteForRoundest(first)} />
+        )}
         <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col items-center">
-          <img src={secondPokemon.data?.sprites.front_default!} className="w-full" />
-          <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
-          <button className={btn} onClick={() => voteForRoundest(first)}>
-            Softer
-          </button>
-        </div>
-        <div className="p-2" />
+        {!firstPokemon.isLoading && firstPokemon.data && !secondPokemon.isLoading && secondPokemon.data && (
+          <PokemonListing pokemon={secondPokemon.data} vote={() => voteForRoundest(second)} />
+        )}
       </div>
     </div>
   );
 }
+
+type PokemonFromServer = inferQueryResponse<'get-pokemon-by-id'>;
+
+const PokemonListing: React.FC<{ pokemon: PokemonFromServer; vote: () => void }> = (props) => {
+  return (
+    <div className="w-64 h-64 flex flex-col items-center">
+      <img src={props.pokemon.sprites.front_default!} className="w-48" />
+      <div className="text-xl text-center capitalize mb-4">{props.pokemon.name}</div>
+      <button className={btn} onClick={() => props.vote()}>
+        Softer
+      </button>
+    </div>
+  );
+};
