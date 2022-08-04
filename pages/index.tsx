16197af84f4e2c3d702ff7a7d65810f26916394f
ryan <ryanslh5@gmail.com>
2022-08-04 10:22:04 -0400

    Basic UI structure on voting page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokémon is softer?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
