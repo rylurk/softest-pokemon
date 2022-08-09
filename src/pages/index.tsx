8c50d1ced03daa8eb685aa2237389c62812e282e
ryan <ryanslh5@gmail.com>
2022-08-09 09:39:23 -0400

    Use next/image loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { getOptionsForVote } from '../utils/getRandomPokemon';
 import { trpc } from '../utils/trpc';
 import { inferQueryResponse } from './api/trpc/[trpc]';
+import Image from 'next/image';
 
 const btn =
   'inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500';
@@ -50,7 +51,7 @@ type PokemonFromServer = inferQueryResponse<'get-pokemon-by-id'>;
 const PokemonListing: React.FC<{ pokemon: PokemonFromServer; vote: () => void }> = (props) => {
   return (
     <div className="w-64 h-64 flex flex-col items-center">
-      <img src={props.pokemon.sprites.front_default!} className="w-48" />
+      <Image src={props.pokemon.sprites.front_default!} height="256" width="256" layout="fixed" />
       <div className="text-xl text-center capitalize mb-4">{props.pokemon.name}</div>
       <button className={btn} onClick={() => props.vote()}>
         Softer
