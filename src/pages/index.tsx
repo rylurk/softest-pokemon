df6767a320feddfc676b564cbde6e2c8c4ff24a9
ryan <ryanslh5@gmail.com>
2022-08-09 16:46:55 -0400

    Update text on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,9 @@ export default function Home() {
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center">Which Pokémon is softer?</div>
+      <div className="text-2xl text-center">
+        Which <span className="text-red-400">Pokémon</span> is softer?
+      </div>
       <div className="mt-2 p-8 flex justify-between max-w-2xl items-center">
         {!firstPokemon.isLoading && firstPokemon.data && !secondPokemon.isLoading && secondPokemon.data && (
           <PokemonListing pokemon={firstPokemon.data} vote={() => voteForRoundest(first)} />
